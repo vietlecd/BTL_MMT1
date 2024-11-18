8570f055d46f3f896438b9f64f0dfe03a885c7f1
vietlecd <120305631+vietlecd@users.noreply.github.com>
2024-11-18 18:04:07 +0700

    init project

---
+++ src/main/java/com/project/btl_mmt1/BtlMmt1Application.java
@@ -0,0 +1,13 @@
+package com.project.btl_mmt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlMmt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtlMmt1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/btl_mmt1/BtlMmt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.btl_mmt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlMmt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
